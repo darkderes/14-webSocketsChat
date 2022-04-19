448f194cf906bb5af5f284ff0e21ff4c3c2b5243
Jorge Darderes <jdarderes@gmail.com>
2022-04-19 17:13:13 -0400

    Codigo final de webchat

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,6 +3,14 @@ const { validarJWT } = require("../../middlewares");
 let usuario = null;
 let socket = null;
 
+//referencias HTML
+
+const txtUid = document.querySelector('#txtUid');
+const txtMensaje = document.querySelector('#txtMensaje');
+const UlMensajes = document.querySelector('#UlUsuarios');
+const UlUsuarios = document.querySelector('#UlMensajes');
+const btnSalir = document.querySelector('#btnSalir');
+
 const validarJWT = async() => {
 
    const token = localStorage.getItem('token') || '';
@@ -23,19 +31,76 @@ const validarJWT = async() => {
 }
 
 const conectarSocket = async() => {
-    const socket = io({ 
+       socket = io({ 
         'extraHeaders' : {
            'x-token': localStorage.getItem('token')
         }
     });
 
+    socket.on('connect',() => {
+        console.log('Sockets online');
+    });
+    socket.on('disconnect',() => {
+        console.log('Sockets offline');
+    }); 
+    socket.on('recibir-mensajes',(payload) => {
+        console.log(payload)
+    });
+    socket.on('usuarios-activos',dibujarUsuarios);
+    socket.on('recibir-mensajes',dibujarMensajes);
+    socket.on('mensaje-privado',( payload ) => {
+        console.log('privado :',payload);
+    });
 }
 
+const dibujarUsuarios = ( usuarios = [])=> {
+    let userHtml = '';
+    usuarios.forEach( ({nombre,uid}) => {
+        users += `
+          <li>
+              <p>
+                  <h5 class = "text-success"> ${ nombre } </h5>
+                  <span class="fs-6 text-muted">${ uid } </span>
+              </p>
+          </li>
+
+        `;
+    });
+
+    UlUsuarios.innerHTML = userHtml;
+}
+
+const dibujarMensajes = ( mensajes = [])=> {
+    let mensajesHtml = '';
+    mensajes.forEach( ({ nombre,mensaje}) => {
+        mensajesHtml += `
+          <li>
+              <p>
+                  <span class = "text-primary"> ${ nombre } </span>
+                  <span>${ mensaje } </span>
+              </p>
+          </li>
+
+        `;
+    });
+
+    UlUsuarios.innerHTML = mensajesHtml;
+}
+
+txtMensaje.addEventListener('keyup',({keyCode}) => {
+
+    const mensaje = txtMensaje.value;
+
+    if(keyCode !== 13){ return; }
+    if(mensaje.length === 0 ) { return;}
+
+    socket.emit('enviar-mensaje',{mensaje,uid});
+    txtMensaje.value = '';
+})
+
+
 const main =  async () => {
-
     await validarJWT();
-
 } 
-
 main();
 //const socket = io();
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,14 +1,38 @@
 const { Socket } = require('socket.io');
 const { comprobarJWT } = require('../helpers');
+const { ChatMensajes } = require('../models');
 
-const socketController = async(socket = new Socket) => {
+const chatMensajes =  new ChatMensajes();
+
+const socketController = async(socket = new Socket(), io) => {
 
    const usuario = await comprobarJWT(socket.handshake.headers['x-token']);
    if(!usuario){
        return socket.disconnect();
    }
-   console.log('se conecto',usuario.nombre);
+   // agregar al usuario
+   chatMensajes.conectarUsuario(usuario);
+   io.emit('usuarios-activos',chatMensajes.usuariosArr);
 
+   // conectarlo a una sala especial 
+
+   socket.join(usuario.id);
+
+
+   // limpiar cuando alguien se desconecte
+   socket.on('disconnect',() =>{
+       chatMensajes.desconectarUsuario(usuario.id);
+       io.emit('usuarios-activos',chatMensajes.usuariosArr);
+   });
+
+   socket.on('enviar-mensaje',({uid,mensaje}) =>{
+
+    if( uid ){
+       socket.to(uid).emit('mensaje-privado',{ de: usuario.nombre, mensaje });  
+    }else{
+       chatMensajes.enviarMensaje(usuario.uid, usuario.nombre,mensaje);
+       io.emit('recibir-mensajes',chatMensajes.ultimos10);  
+    }    });
 }
 module.exports = {
     socketController
